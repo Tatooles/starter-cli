0c1ef1b13ffddf04e6adff8e2d083043775f800d
Kevin Tatooles <tatooles@wisc.edu>
2023-09-29 21:30:13 -0500

    Add name function

--- index.js
+++ index.js
@@ -6,3 +6,40 @@ import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    "Welcome to the ultimate sports quiz! \n"
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`
+    ${chalk.bgBlue("HOW TO PLAY")}
+    I am a process on your computer
+    If you get any questions wrong I will be ${chalk.bgRed("killed")}
+    So get all the questions right...
+  
+  `);
+}
+
+async function askName() {
+  const answers = await inquirer.prompt({
+    name: "player_name",
+    type: "input",
+    message: "What is your name?",
+    default() {
+      return "Player";
+    },
+  });
+
+  playerName = answers.player_name;
+}
+
+// await welcome();
+// await askName();
